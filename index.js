30837d1d00944db3c72f4c59ba8452a989f1f5cd
Anton Ivanov <anton@ivanov.hk>
2018-07-23 02:18:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const THIS_FUNCTION_NAME = 'ycomb-poster';
 // Telegram settings
 const NEWS_URL = 'https://news.ycombinator.com/';
 const SILENT_BROADCAST = true;
-const TELEGRAM_BOT_TOKEN = <INSERT TELEGRAM BOT TOKEN FROM @BOTFATHER HERE>;
+const TELEGRAM_BOT_TOKEN = <INSERT TELEGRAM BOT TOKEN FROM @botfather HERE>;
 const TELEGRAM_CHANNEL = '@news_ycombinator';
 const TELEGRAM_POST_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHANNEL}&parse_mode=Markdown${SILENT_BROADCAST ? '&disable_notification=true' : '' }&text=`;
 
 // Hacker News settings:
+const COMMENTS_SELECTOR = '.itemlist .subtext > a:last-child';
 const NEWS_CONTAINER_SELECTOR = '.itemlist';
 const NEWS_ITEM_SELECTOR = '.athing';
 const NEWS_LINK_SELECTOR = 'a.storylink';
 const SCORE_SELECTOR = '.score';
 const SUBTEXT_SELECTOR = '.itemlist .subtext';
-const MIN_VOTE_REQUIRED = '20';
+const MIN_VOTE_REQUIRED = 20;
 
 // Other settings:
 const SENT_ITEMS_FILENAME = `./${THIS_FUNCTION_NAME}/sent.json`;
@@ -35,7 +36,8 @@ module.exports = async function (context, myTimer) {
         const $ = cheerio.load(html);
         const $articles = getArticles($);
         const votes = collectVoteCounts($);
-        const newsItems = extractNewsFromHtml($, $articles, votes);
+        const {links, comments} = collectCommentLinks($);
+        const newsItems = extractNewsFromHtml($, $articles, votes, links, comments);
         
         context.log(`Fetched ${newsItems.length} articles.`);
 
@@ -89,20 +91,36 @@ function collectVoteCounts($) {
     return votes;
 }
 
-function extractNewsFromHtml($, $articles, votes) {
+function collectCommentLinks($) {
+    let links = [];
+    let comments = []
+    $(COMMENTS_SELECTOR).each((i, element) => {
+        links.push(NEWS_URL + $(element).attr('href'));
+        comments.push($(element).text());
+    });
+    return { links, comments };
+}
+
+function extractNewsFromHtml($, $articles, votes, links, comments) {
     let news = [];
     $articles.each(function(i, article) {
         let $link = $(article).find(NEWS_LINK_SELECTOR);
         let url = $link.attr('href');
         let title = $link.text();
         let score = votes[i];
-        news.push({url, title, score});
+        let commentLink = links[i];
+        let commentCount = comments[i];
+
+        // Escape _, otherwise it turns to italic:
+        url = url.replace(/_/g, '\\_');
+
+        news.push({url, title, score, commentLink, commentCount});
     });
     return news;
 }
 
 function generateMessage(item) {
-    return `*${item.title}*\r\n${item.url}`;
+    return `*${item.title}*\r\n${item.url} | [${item.commentCount}](${item.commentLink})`;
 }
 
 function selectNewItemsOnly(log, items) {
@@ -126,7 +144,9 @@ async function sendMessageToTelegram(message) {
 }
 
 function markItemSent(log, item) {
-    // Date is saved to later clean up old log records:
+    // Date is saved to later clean up old log records.
+    // To avoid resending the same item in case of error,
+    // we immediately save log to disk.
     log.sent[item.url] = Date.now();
     fs.writeFileSync(SENT_ITEMS_FILENAME, JSON.stringify(log));
 }
